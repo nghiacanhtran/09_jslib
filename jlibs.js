7de9aca54fe1cd900d5227aea8dfaa2534399bbc
nghia tran <nghiacanhtran@gmail.com>
2016-07-20 10:53:56 +0700

    update code 20/7/2016

--- jlibs.js
+++ jlibs.js
@@ -86,10 +86,10 @@ jfpts.libs = jfpts.libs || {};
      * @param valueRemove giá trị của thuộc tính
      * @example jfpts.libs.removeObjectInArray([{id: 1,name:'nghia'},{id:2,name:'b'}],'id',1) = >[{id:2,name:'b'}]
      */
-    this.removeObjectInArray = function (array,propertyNameObject,valueRemove) {
+    this.removeObjectInArray = function (array, propertyNameObject, valueRemove) {
         if (array.length > 0) {
             return $.grep(array, function (n, i) {
-                return n[propertyNameObject] != valueRemove;
+                return n[propertyNameObject] !== valueRemove;
             });
         }
     };
@@ -267,7 +267,7 @@ jfpts.libs = jfpts.libs || {};
             });
         }
 
-        return newValue
+        return newValue;
     };
 
     /**
@@ -276,7 +276,7 @@ jfpts.libs = jfpts.libs || {};
      * @param {string} name - Tên của query string.
      * @return {string || null}
      */
-    this.getURLParameter = function(name) {
+    this.getURLParameter = function (name) {
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
     }
 
@@ -299,5 +299,166 @@ jfpts.libs = jfpts.libs || {};
         return str.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
     };
 
+    /**
+    * @author nghiatc.
+    * @description Sinh ra chuỗi random.
+    * @example 
+    * th1 : jfpts.libs.generateStringRandom()
+    */
+    this.generateStringRandom = function () {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+            currentDate = new Date(),
+            ranNums = [],
+            i = chars.length,
+            j = 0;
 
+        while (i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            ranNums.push(chars[j]);
+        }
+
+        var randomString = ranNums.join('');
+        randomString = currentDate.getMilliseconds() + currentDate.getMonth() + currentDate.getSeconds() + currentDate.getMinutes() + randomString + Math.floor(Math.random() * (1000000 - 1 + 1)) + 1;
+        return randomString;
+    }
+
+    /**
+    * @author nghiatc.
+    * @description Lấy ra secment from urlPathName.
+    * @example 
+    * jfpts.libs.getSegementCurrentPathName(1,'http://partner1048.avumo.dev:8200/rrpeditor/index/2') => 2
+    * @param
+    * postion => vị trí của thành phần muốn lấy tính từ cuối chuỗi rrpeditor/index/2.Vị trí số 1 là 2,vị trí số 2 là index
+    * stringPath => Path name : rrpeditor/index/2
+    */
+    this.getSegementCurrentPathName = function (postion, stringPath) {
+        var secmentReturn = '';
+        if ($.trim(stringPath) !== '') {
+            var arraySecment = stringPath.split('/');
+
+            if (arraySecment.length > 0) {
+
+                secmentReturn = arraySecment[arraySecment.length - Number(postion)];
+            }
+        }
+        return secmentReturn;
+    }
+
+    /**
+     * @author nghiatc
+     * @description validate link định dạng ảnh.
+     * @example 
+     * jfots.jfpts.IsImage("https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/leisa_christmas_false_color.gif")
+     */
+    this.IsImage = function (imageName) {
+        var re = /\bhttps?:[^)''"]+\.(?:jpg|jpeg|gif|png|bmp)(?![a-z/])/igm;
+
+        if (imageName.match(re)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+    * @author nghiatc
+    * @description validate định dạng ảnh.
+    * @example 
+    * jfots.jfpts.isValidImage("leisa_christmas_false_color.gif")
+    */
+    this.isValidImage = function(imageName) {
+        var re = /\.(?:jpg|jpeg|gif|png|bmp)(?![a-z\/])/igm;
+
+        if (imageName.match(re)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * @author nghiatc
+     * @description Check if string is html tag
+     * @example 
+     * jfpts.libs.isHtmlTag("<p>")
+     */
+    this.isHtmlTag = function (stringTag) {
+        var re = /<[a-z][\s\S]*>/igm;
+
+        if (stringTag.match(re)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+    * @author nghiatc
+    * @description prevent render html tag to dom
+    * @example 
+    * jfpts.libs.preventRenderHtml("<input/>") => <input/>
+    */
+    this.preventRenderHtml = function (value) {
+        var tempHtml = value.replace(/\</igm, '&lt;');
+        tempHtml = tempHtml.replace(/\/>/igm, '&gt;');
+        return tempHtml;
+    }
+
+    /**
+   * @author nghiatc
+   * @description decode html
+   * @example 
+   * jfpts.libs.decodeHtml("&lt;input&gt;") => <input/>
+   */
+    this.decodeHtml = function (value) {
+        if (jfpts.libs.isHtmlTag(value) === false) {
+            var tempHtml = $("<div></div>").html(value).text();
+            return tempHtml;
+        }
+        return value;
+    }
+
+    /**
+     * @author nghiatc
+     * @description Hàm đếm số lần suốt hiện của chuỗi con trong chuỗi gốc.
+     */
+    this.countOcurrentce = function(str, matchValue) {
+        var rexEx = new RegExp(matchValue, "igm");
+        return (str.match(rexEx) || []).length;
+    }
+
+    /**
+    * @author Nghiatc2.
+    * @description: Kiểm tra email có hợp lệ
+    * @param {string} stringTest chuỗi cần kiểm tra
+    * @example:
+    * if (jfpts.libs.isEmail()) {Đúng định dạng}
+    * @return {boolean}
+    */
+    this.isEmail = function(stringTest) {
+        var regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+        if (stringTest.match(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+    * @author Nghiatc2.
+    * @description: Kiểm tra url có hợp lệ
+    * @param {string} stringTest chuỗi cần kiểm tra
+    * @example:
+    * if (jfpts.libs.isUrl()) {Đúng định dạng}
+    * @return {boolean}
+    */
+    this.isUrl = function(stringTest) {
+        var regex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+        if (stringTest.match(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }).apply(jfpts.libs);
